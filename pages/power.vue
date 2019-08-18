38498e07ebb0d2abfb32cb8c1f3ad1a5237236a9
Michał Maćkowiak <michal.mackowiak@netguru.pl>
2019-08-18 12:29:25 +0200

    Convert power to new version

--- pages/power.vue
+++ pages/power.vue
@@ -2,16 +2,58 @@
   v-layout.row
     v-card.elevation-5
       .function__header {{ name }}
-      v-layout.part.justify-center.row.pa-2
-        .argument(v-for="param in params")
-          .argument__header {{ param.name }}
-          .argument__value {{ param.value }}
-      v-layout.part.justify-center.row.pa-2
-        .column(v-for="column in columns")
-          .column__header {{ column.name }}
-          .column__value(v-for="cell in column.cells", :class="cell.color") {{ cell.value }}
-      .function__header result
-      v-layout.part.justify-center.row.pa-2.result {{ result }}
+      v-layout.row.justify-center.pa-3
+        table.argument(v-for="argument in params")
+          template(v-if="argument.type === 'sequence'")
+            tr
+              th(:colspan="argument.values.length") {{ argument.name }}
+            tr
+              td(v-for="value in argument.values") &nbsp;
+          template(v-else)
+            tr
+              th {{ argument.name }}
+            tr
+              td &nbsp;
+      v-layout.part.row.justify-center.pa-3
+        table.board
+          tr
+            th(v-for="column in columns" :class="column.class") {{ column.name }}
+          tr(v-for="(_, i) in columns[0].cells")
+            template(v-for="column in columns")
+              td(
+                v-if="!column.cells[i].skip"
+                :rowspan="column.cells[i].span"
+                :height="column.cells[i].span*30 + 'px'"
+                :class="[column.class, column.cells[i].color]"
+              )
+                span(v-if="column.cells[i].expression") =&thinsp;{{ column.cells[i].expression }}
+                span(v-else) &nbsp;
+      .function__footer result
+      .result =&thinsp;{{ result.expression }}
+
+    v-card.example.elevation-5
+      .function__header Example
+      v-layout.row.justify-center.pa-3
+        table.argument(v-for="argument in params")
+          template(v-if="argument.type === 'sequence'")
+            tr
+              th(:colspan="argument.values.length") {{ argument.name }}
+            tr
+              td(v-for="value in argument.values") {{ value }}
+          template(v-else)
+            tr
+              th {{ argument.name }}
+            tr
+              td {{ argument.value }}
+      v-layout.part.row.justify-center.pa-3
+        table.board
+          tr
+            th(v-for="column in columns" :class="column.class") {{ column.name }}
+          tr(v-for="(_, i) in columns[0].cells")
+            template(v-for="column in columns")
+              td(:class="[column.class, column.cells[i].color]") {{ column.cells[i].value }}
+      .function__footer result
+      .result {{ result.value }}
 </template>
 
 <script>
@@ -36,34 +78,39 @@ export default {
     return {
       name: 'Power',
       params: [
-        { name: 'a', value: '2' },
-        { name: 'n', value: '5' }
+        { name: 'a', type: 'value', value: '2' },
+        { name: 'n', type: 'value', value: '5' }
       ],
       columns: [
         {
           name: 'i',
+          class: 'standard-column',
           cells: [
-            { value: 0 },
-            { value: 1, color: 'index-slot' },
-            { value: 2, color: 'index-slot' },
-            { value: 3, color: 'index-slot' },
-            { value: 4, color: 'index-slot' },
-            { value: 5, color: 'index-slot' }
+            { value: 0, expression: '0' },
+            { value: 1, expression: '$+1', span: 4, color: 'index-slot' },
+            { value: 2, color: 'index-slot', skip: true },
+            { value: 3, color: 'index-slot', skip: true },
+            { value: 4, color: 'index-slot', skip: true },
+            { value: 5, color: 'index-slot', expression: 'n' }
           ]
         },
         {
           name: 'Value',
+          class: 'standard-column',
           cells: [
-            { value: 1 },
-            { value: 2, color: 'blue-slot' },
-            { value: 4, color: 'blue-slot' },
-            { value: 8, color: 'blue-slot' },
-            { value: 16, color: 'blue-slot' },
-            { value: 32, color: 'blue-slot' }
+            { value: 1, expression: '1' },
+            { value: 2, expression: '$*a', span: 5, color: 'blue-slot' },
+            { value: 4, skip: true, color: 'blue-slot' },
+            { value: 8, skip: true, color: 'blue-slot' },
+            { value: 16, skip: true, color: 'blue-slot' },
+            { value: 32, skip: true, color: 'blue-slot' }
           ]
         }
       ],
-      result: 32
+      result: {
+        expression: 'Last(Value)',
+        value: 32
+      }
     }
   }
 }
@@ -72,54 +119,87 @@ export default {
 <style lang="stylus" scoped>
 .function__header
   background-color: #558b2f
+  border: 1px solid black
   color: white
   font-size: 1.5rem
   font-weight: 600
-  padding: 0.25rem 0.5rem
-  border: 1px solid black
+  padding: 0.3rem 0.5rem
   text-align: center
 
-.part
-  border: 1px solid #a7a7a7
+.function__footer
+  background-color: #558b2f
+  border: 1px solid black
+  color: white
+  font-size: 1.2rem
+  font-weight: 600
+  padding: 0.3rem 0.5rem
+  text-align: center
 
 .argument
+  border-collapse: collapse
   font-size: 1.2rem
   text-align: center
-  margin: 5px
-  min-width: 2.5rem
+  margin-right: 1rem
 
-  &__header
+  &:last-child
+    margin: 0rem
+
+  th
     background-color: #616161
-    color: white
-    padding: 0.1rem 0.5rem
     border: 1px solid black
+    color: white
     font-weight: bold
+    padding: 0.3rem 0.5rem
 
-  &__value
+  td
     background-color: white
-    padding: 0.25rem 0.5rem
     border: 1px solid #a7a7a7
-    text-align: center
+    min-width: 2.5rem
+    padding: 0.3rem 0.5rem
 
-.column
-  font-size: 1.2rem
+.board
+  background-color: white
+  border-collapse: collapse
   text-align: center
-  margin: 5px
-  min-width: 2.5rem
+  margin-right: 1rem
 
-  &__header
-    background-color: #e0e0e0
-    color: black
-    padding: 0.1rem 0.5rem
-    border: 1px solid #a7a7a7
+  &:last-child
+    margin: 0rem
+
+  th
+    font-size: 1.2rem
     font-weight: bold
+    padding: 0.3rem 0.5rem
 
-  &__value
-    margin-top: -1px
-    background-color: white
-    padding: 0.25rem 0.5rem
+    &.standard-column
+      background-color: #e0e0e0
+      border: 1px solid #a7a7a7
+      color: black
+
+    &.shadow-column
+      background-color: #616161
+      border: 1px dashed #a7a7a7
+      color: white
+
+  td
+    padding: 0.3rem 0.5rem
     border: 1px solid #a7a7a7
-    text-align: center
+    min-width: 2.5rem
+    padding: 0.3rem 0.5rem
+
+    &.standard-column
+      border: 1px solid #a7a7a7
+
+    &.shadow-column
+      border: 1px dashed #a7a7a7
+
+.part
+  border-top: 1px solid #a7a7a7
+
+.result
+  font-size: 1.2rem
+  padding: 0.7rem 0.5rem
+  text-align: center
 
 .index-slot
   background-color: #ffe172
@@ -127,6 +207,16 @@ export default {
 .blue-slot
   background-color: #b3dffd
 
-.result
-  font-size: 1.2rem
+.pink-slot
+  background-color: #ffcdd3
+
+.example
+  margin-left: 50px
+  background-color: #fff8e2 !important
+
+  .function__header
+    background-color: #7e5f01
+
+  .function__footer
+    background-color: #7e5f01
 </style>
